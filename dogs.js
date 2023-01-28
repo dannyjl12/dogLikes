5b1d15afc98b33c3332beb8502a051cb6c3b1888
danny-lowton <dannylowton@gmail.com>
2023-01-28 14:43:00 -0500

    Added information on what dogs like

---
+++ dogs.js
@@ -0,0 +1,10 @@
+//this program will tell us about the best dogs in the world
+
+let dogs = [
+    {name: 'Cooper', likes: 'sleeping'},
+    {name: 'Charlie', likes: 'being a demon'}
+]
+
+for (let i=0; i < dogs.length; i++) {
+    console.log(dogs[i].name + ' likes ' + dogs[i].likes);
+}
